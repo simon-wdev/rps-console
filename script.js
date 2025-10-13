37e9bbdaa230f9274a7246008392dd8cbb97eb61
simonw-dev <214550778+simon-wdev@users.noreply.github.com>
2025-10-13 10:30:56 +0200

    Add comments
    Added comments for better understanding.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let npcPick = "";
 let playerPick;
 
+//Computer choice gets picked based on random picked numbers (0, 1, 2)
+//0 == Rock, 1 == Paper, 2 == Scissors
+// The choice will be stored global in "npcPick"
 function getComputerChoice(){
     let choice = Math.floor(Math.random()*3)
     if (choice == 0){
@@ -12,9 +15,11 @@ function getComputerChoice(){
     }
 };
 
+//Player can pick between Rock Paper, Scissors and has to type it into the prompt.
+//If player choice is written wrong he will get an error alert to enter choice again
+//Everything is transformed into lower case, so it is only important to write words correctly
 function getPlayerChoice(){
     let playerChoice = prompt("Pick Rock, Paper or Scissors!").toLowerCase();
-    //playerChoice = playerChoice.toLowerCase();
     if (playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors"){
         alert("Thats not valid. Please enter Rock, Paper or Scissors!")
         getPlayerChoice();
