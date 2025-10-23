aa16103c21c279c7cfbce3d7724f9289ae562a69
simonw-dev <214550778+simon-wdev@users.noreply.github.com>
2025-10-23 14:25:16 +0200

    Add some style and labels
    A round winner will now show up, when clicking on a button. Added some basic css.

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+
 let playerScore = 0;
 let npcScore = 0;
+let result = document.createElement("p");
+let scoreRound = document.querySelector(".scoreRound")
+
 const playerRock = document.querySelector(".btnRock");
 const playerPaper = document.querySelector(".btnPaper");
 const playerScissors = document.querySelector(".btnScissors");
-const res = document.querySelector(".res");
+const resBox = document.querySelector(".res");
 
 playerRock.addEventListener("click", function(){
     playRound(getComputerChoice(), "rock");
@@ -32,56 +36,44 @@ function getComputerChoice(){
     }
 };
 
-//Player can pick between Rock Paper, Scissors and has to type it into the prompt.
-//If player choice is written wrong he will get an error alert to enter choice again
-//Everything is transformed into lower case, so it is only important to write words correctly
-function getPlayerChoice(){
-    let playerChoice = prompt("Pick Rock, Paper or Scissors!").toLowerCase();
-    if (playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors"){
-        alert("Thats not valid. Please enter Rock, Paper or Scissors!")
-        getPlayerChoice();
-    }
-    return playerPick = playerChoice;
-};
-
 //Logic for a single round, two equals are a tie, rock beats scissors etc. and vice versa
 //Function returns the winner score at the end of each statement
 //If the round is a tie, no score will be returned
     function playRound(npcPick, playerPick){
         if(playerPick === npcPick){
-            console.log(`NPC picked ${npcPick}.`);
-            console.log("Tie! Nobody wins!");
+            resBox.appendChild(result);
+            result.textContent = `NPC picked ${npcPick}.\nTie! Nobody wins!`;
             return;
         }
 
         if(playerPick === "rock"){
             if(npcPick === "paper"){
-                console.log(`NPC picked ${npcPick}.`);
-                console.log("You lose! Paper beats rock.");
+                resBox.appendChild(result);
+                result.textContent = `NPC picked ${npcPick}.\nYou lose! Paper beats rock.`;
                 npcScore += 1;
             }else{
-                console.log(`NPC picked ${npcPick}.`)
-                console.log("You win! Rock beats scissors.");
+                resBox.appendChild(result);
+                result.textContent = `NPC picked ${npcPick}.\nYou win! Rock beats scissors.`;
                 playerScore += 1;
             } 
         }else if(playerPick === "paper"){
             if(npcPick === "scissors"){
-                console.log(`NPC picked ${npcPick}.`)
-                console.log("You lose! Scissors beat paper.");
+                resBox.appendChild(result);
+                result.textContent = `NPC picked ${npcPick}.\nYou lose! Scissors beats paper.`;
                 npcScore += 1;
             }else{
-                console.log(`NPC picked ${npcPick}.`)
-                console.log("You win! Paper beats rock.");
+                resBox.appendChild(result);
+                result.textContent = `NPC picked ${npcPick}.\nYou win! Paper beats rock.`;
                 playerScore += 1;
             }
         }else if(playerPick === "scissors"){
             if(npcPick == "rock"){
-                console.log(`NPC picked ${npcPick}.`)
-                console.log("You lose! Rock beats scissors");
+                resBox.appendChild(result);
+                result.textContent = `NPC picked ${npcPick}.\nYou lose! Rock beats scissors.`;
                 npcScore += 1;
             }else{
-                console.log(`NPC picked ${npcPick}.`)
-                console.log("You win! Scissors beat paper!");
+                resBox.appendChild(result);
+                result.textContent = `NPC picked ${npcPick}.\nYou win! Scissors beat paper.`;
                 playerScore += 1;
             }
         }
